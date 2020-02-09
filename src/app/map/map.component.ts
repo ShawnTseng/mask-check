addfebaf50439fe23e8b6c64a24c0b54c6e1b143
shawntseng <shawntseng40@gmail.com>
2020-02-09 11:53:51 +0800

    重構

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,47 +10,55 @@ import 'leaflet.markercluster';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent {
+  /** 藥局位置叢聚 */
   markerCluster: Array<Marker> = [];
 
+  /** 主地圖 */
   private map: Map;
 
   constructor(private http: HttpClient) { }
 
   onMapReady(map: Map) {
-    this.map = map;
+    this.initMap(map);
     this.getUserLocation();
+    this.loadPhamacyData();
+  }
+
+  private initMap(map: Map) {
+    this.map = map;
+    // 加入地圖資料
+    this.map.addLayer(tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }));
   }
 
   /** 取得使用者位置 */
   private getUserLocation() {
     const geoLocation: Geolocation = navigator.geolocation;
     if (geoLocation) {
-      geoLocation.getCurrentPosition(this.positionCallback.bind(this));
+      geoLocation.getCurrentPosition(this.initPanToUserPosition.bind(this), this.initPanToDafault.bind(this));
     } else {
       console.log('Geolocation is not supported.');
     }
   }
 
-  private positionCallback(position: Position) {
-    this.initMap(position.coords);
-  }
-
-  private initMap({ latitude, longitude }: Coordinates) {
-    this.map.addLayer(tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    }));
-    this.map.setZoom(17);
-    this.map.panTo(new LatLng(latitude, longitude));
-
-    // 自己的位置
+  /** 定位使用者位置 */
+  private initPanToUserPosition({ coords: { latitude, longitude } }: Position) {
     const selfMark = new Marker(new LatLng(latitude, longitude));
     const selfPopup = new Popup({ closeButton: false }).setContent('<div style="text-align: center;">You</div>');
     selfMark.bindPopup(selfPopup).openPopup();
     selfMark.addTo(this.map);
-
-    this.loadPhamacyData();
+    this.map.setZoom(17);
+    this.map.panTo(new LatLng(latitude, longitude));
   }
 
+  /** 使用者定位失敗, 預設定位台灣 */
+  private initPanToDafault() {
+    this.map.setZoom(7);
+    this.map.panTo(new LatLng(23.5311922, 120.7534645));
+  }
+
+  /** 載入藥局位置 */
   private loadPhamacyData() {
     this.http.get('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json')
       .subscribe((response: PhamacyPointResponse) => {
