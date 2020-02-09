3f907d32c455854a37f29a08b27d305d05ee7537
shawntseng <shawntseng40@gmail.com>
2020-02-09 10:46:31 +0800

    新增api資料說明

--- src/app/map/phamacy-point-response.ts
+++ src/app/map/phamacy-point-response.ts
@@ -16,16 +16,37 @@ export interface PhamacyPoint {
 }
 
 export interface PhamacyPointProperty {
+  /** 醫事機構代碼 */
   id: string;
+
+  /** 醫事機構名稱 */
   name: string;
+
+  /** 醫事機構電話 */
   phone: string;
+
+  /** 醫事機構地址 */
   address: string;
+
+  /** 成人口罩總剩餘數 */
   mask_adult: number;
+
+  /** 兒童口罩剩餘數 */
   mask_child: number;
+
+  /** 來源資料時間	 */
   updated: Date;
+
+  /** 固定看診時段 */
   available: string;
+
+  /** 備註 */
   note: string;
+
+  /** 口罩銷售提醒 */
   custom_note: string;
+
+  /** 網站 */
   website: string;
 }
 
